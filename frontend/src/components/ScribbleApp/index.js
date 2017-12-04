818a78c519101670036a5593271d9ba8b905445a
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2017-12-04 14:09:38 -0800

    Implement beginning features

--- frontend/src/components/ScribbleApp/index.js
+++ frontend/src/components/ScribbleApp/index.js
@@ -7,6 +7,13 @@ export default class ScribbleApp extends React.Component {
   render() {
     return (
       <div className='scribble-app-container'>
+        <div className="header">
+          Scribble.today
+          <div className="subtitle">
+            A place to confess, anonymously.
+          </div>
+        </div>
+
         <LeftPane />
         <RightPane />
       </div>
